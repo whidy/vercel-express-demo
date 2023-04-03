652bc976434da95ef08ad6141842792550fdd1f6
baishun <baishun@wodcloud.com>
2023-04-03 12:48:12 +0800

    public

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const port = 3200
+app.use(express.static('public'))
+
 
 app.get('/', (req, res) => {
   res.send('The first Hello World!')
