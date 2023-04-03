bd19d6f203e91ae391a17675e4c50d9af7092f4c
baishun <baishun@wodcloud.com>
2023-04-03 12:33:48 +0800

    api

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 // https://vercel.com/guides/using-express-with-vercel#standalone-express
 const app = require('express')();
 
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
   res.send("Hello World!");
 });
 
